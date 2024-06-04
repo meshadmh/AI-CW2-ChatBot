e9018a6ef2e268f41fc46c3a0241e3adeb88384f
meshadmh <147321583+meshadmh@users.noreply.github.com>
2024-06-04 15:59:47 +0100

    Update Webscraping.py
    updated click feedback function

--- Task_1/Webscraping.py
+++ Task_1/Webscraping.py
@@ -253,7 +253,7 @@ def click_show_trains(driver):
 
 def click_feedback(driver):
     try:
-        click_no = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'fsrInvite')))
+        click_no = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="fsrInvite"]/section[3]/button[2]')))
         time.sleep(5)
         click_no.click()
         time.sleep(3)
